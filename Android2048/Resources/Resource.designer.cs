bfb1669965ac7d11509c397631f908604204b3fa
Shaked Sisso <sissoshaked@gmail.com>
2023-06-02 19:02:44 +0300

    Added the swipe detector to the app and implemented the game changes according to the swipe direction

--- Android2048/Resources/Resource.designer.cs
+++ Android2048/Resources/Resource.designer.cs
@@ -4167,37 +4167,40 @@ namespace Android2048
 			public const int txtScore = 2131230968;
 			
 			// aapt resource value: 0x7F0800F9
-			public const int @unchecked = 2131230969;
+			public const int txtWinOrLose = 2131230969;
 			
 			// aapt resource value: 0x7F0800FA
-			public const int uniform = 2131230970;
+			public const int @unchecked = 2131230970;
 			
 			// aapt resource value: 0x7F0800FB
-			public const int unlabeled = 2131230971;
+			public const int uniform = 2131230971;
 			
 			// aapt resource value: 0x7F0800FC
-			public const int up = 2131230972;
+			public const int unlabeled = 2131230972;
 			
 			// aapt resource value: 0x7F0800FD
-			public const int useLogo = 2131230973;
+			public const int up = 2131230973;
 			
 			// aapt resource value: 0x7F0800FE
-			public const int view_offset_helper = 2131230974;
+			public const int useLogo = 2131230974;
 			
 			// aapt resource value: 0x7F0800FF
-			public const int view_tree_saved_state_registry_owner = 2131230975;
+			public const int view_offset_helper = 2131230975;
 			
 			// aapt resource value: 0x7F080100
-			public const int visible = 2131230976;
+			public const int view_tree_saved_state_registry_owner = 2131230976;
 			
 			// aapt resource value: 0x7F080101
-			public const int visible_removing_fragment_view_tag = 2131230977;
+			public const int visible = 2131230977;
 			
 			// aapt resource value: 0x7F080102
-			public const int withText = 2131230978;
+			public const int visible_removing_fragment_view_tag = 2131230978;
 			
 			// aapt resource value: 0x7F080103
-			public const int wrap_content = 2131230979;
+			public const int withText = 2131230979;
+			
+			// aapt resource value: 0x7F080104
+			public const int wrap_content = 2131230980;
 			
 			static Id()
 			{
